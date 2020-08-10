3449fc919c39e834a7616fa4e2f243bf7d9da7c5
Gabriel Meng <menggabriel@berkeley.edu>
2020-08-09 17:28:53 -0700

    [feat] added migrations

---
+++ db/migrate/20200810001753_add_cols_to_representatives.rb
@@ -0,0 +1,10 @@
+class AddColsToRepresentatives < ActiveRecord::Migration[5.2]
+  def change
+    add_column :representatives, :street, :string
+    add_column :representatives, :city, :string
+    add_column :representatives, :state, :string
+    add_column :representatives, :zip, :string
+    add_column :representatives, :party, :string
+    add_column :representatives, :photo, :string
+  end
+end
