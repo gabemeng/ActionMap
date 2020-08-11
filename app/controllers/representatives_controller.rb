94e2f3802616bf1bf2a6942ee267e9bf67ffa342
Gabriel Meng <menggabriel@berkeley.edu>
2020-08-10 22:04:00 -0700

    [feat] repProfiles

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
+require 'google/apis/civicinfo_v2'
+
 class RepresentativesController < ApplicationController
     def index
         @representatives = Representative.all
+    end
+
+    def show
+        id = params[:id]
+        @rep = Representative.find_by(id: id)
     end
 end
--- app/models/representative.rb
+++ app/models/representative.rb
@@ -9,6 +9,8 @@ class Representative < ApplicationRecord
         rep_info.officials.each_with_index do |official, index|
             ocdid_temp = ''
             title_temp = ''
+            temp_street = ''
+            temp_city = ''
 
             rep_info.offices.each do |office|
                 if office.official_indices.include? index
@@ -17,11 +19,35 @@ class Representative < ApplicationRecord
                 end
             end
 
-            rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-                                           title: title_temp })
+            rep = get_data(official, title_temp, ocdid_temp, temp_street, temp_city)
             reps.push(rep)
         end
 
         reps
+    end
+
+    def self.get_data(official, title, temp_id, temp_street, temp_city)
+        temp_state = ''
+        temp_zip = ''
+        # official.address returns a list
+        return no_address(official, title, temp_id) if official.address.nil?
+
+        official.address.each do |addy|
+            temp_street = addy.line1
+            temp_city = addy.city
+            temp_state = addy.state
+            temp_zip = addy.zip
+        end
+        Representative.create!({ name: official.name, ocdid: temp_id,
+                    title: title, party: official.party, photo: official.photo_url,
+                    street: temp_street, city: temp_city, state: temp_state,
+                    zip: temp_zip })
+    end
+
+    def self.no_address(official, title, temp_id)
+        Representative.create!({ name: official.name, ocdid: temp_id,
+                    title: title, party: official.party, photo: official.photo_url,
+                    street: '', city: '', state: '',
+                    zip: '' })
     end
 end
