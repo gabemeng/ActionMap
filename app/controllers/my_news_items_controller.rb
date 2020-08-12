d72628358e059b38c030bf6430fcf5699922f588
Gabriel Meng <menggabriel@berkeley.edu>
2020-08-11 19:49:25 -0700

    [feat] news issues done

--- app/controllers/my_news_items_controller.rb
+++ app/controllers/my_news_items_controller.rb
@@ -40,11 +40,12 @@ class MyNewsItemsController < SessionController
     private
 
     def set_issues_list
-        @issues_list = ["Free Speech", "Immigration", "Terrorism", "Social Security and Medicare", 
-        "Abortion", "Student Loans", "Gun Control", "Unemployment", "Climate Change", 
-        "Homelessness", "Racism", "Tax Reform", "Net Neutrality", "Religious Freedom", 
-        "Border Security", "Minimum Wage", "Equal Pay"]
+        @issues_list = ['Free Speech', 'Immigration', 'Terrorism', 'Social Security and Medicare',
+                        'Abortion', 'Student Loans', 'Gun Control', 'Unemployment', 'Climate Change',
+                        'Homelessness', 'Racism', 'Tax Reform', 'Net Neutrality', 'Religious Freedom',
+                        'Border Security', 'Minimum Wage', 'Equal Pay']
     end
+
     def set_representative
         @representative = Representative.find(
             params[:representative_id]
